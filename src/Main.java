0485e030738f6251c3f1a9a8822026a8d473ad37
Oliver Miguez Alonso <omiguezalonso@danielcastelao.org>
2025-01-29 13:40:28 +0100

    Commit main

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Práctica pull-request
+ * @author Oliver y Dima
+ * @version  1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Esto es la rama main, donde introduciremos cada unda de las clases");
+    }
+}
